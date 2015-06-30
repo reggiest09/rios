5906978b011d715de765ac7c99cf13ad5a3ef344
Reggie Tucker <reggiest09@gmail.com>
2015-06-30 02:35:07 -0400

    added validations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,37 @@ class ContactsController < ApplicationController
 	end
 
 	def create
-	
+	@contact = Contact.new(contact_params)
+   
+    if @contact.save
+    	flash[:success] = 'message sent'
+    redirect_to new_contact_path
+    else
+     flash[:danger] = 'error happened'
+      redirect_to new_contact_path 
+  end
 	end
 
+private
+
+def contact_params
+params.require(:contact).permit(:name, :email, :comments)	
+
 end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
