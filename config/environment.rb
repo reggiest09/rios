bb6f5d2365afa36f6a56cd1c3848e0fbb7343be4
Reggie Tucker <reggiest09@gmail.com>
2015-07-05 02:05:27 -0400

    fix fx

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :authenication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password =>   ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com'
+  :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
 
