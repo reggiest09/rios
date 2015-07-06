463d94b2a786d8cc41b6487fee95e42445185a8d
Reggie Tucker <reggiest09@gmail.com>
2015-07-06 14:29:02 -0400

    fixed stripe typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :configure_permited_parameters, if: :devise_controller?
  
  protected
  def configure_permited_parameters
-devise_paramater_sanitizer.for(:sign_up) {|u| u.permit(:name, :stripe_card_token. :email, :password, :password_conformation)}
+devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :stripe_card_token, :email, :password, :password_conformation)}
 
  end
 
