0c37e3e8779646cfe01356cf966cb15f159d9d43
Reggie Tucker <reggiest09@gmail.com>
2015-07-07 02:49:45 -0400

    change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
-class Users::RegistrationsController < Devise::RegistrationsController 
-
+class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     super do |resource|
@@ -14,4 +13,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+
+  
 end
