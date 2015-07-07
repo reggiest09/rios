beb70e4c083518533a7e6c3f59f5b88a4f1eccaf
Reggie Tucker <reggiest09@gmail.com>
2015-07-06 22:55:30 -0400

    jfj

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
   // Watch for a form submission:
   $("#form-submit-btn").click(function(event) {
     event.preventDefault();
-    $('input[type=submit]').prop('disabled', true);
+    // $('input[type=submit]').prop('disabled', true);
     var error = false;
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -21,17 +20,13 @@ $(document).ready(function() {
     }
     return false;
   }); // form submission
-  
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
-
     // Get the token from the response:
     var token = response.id;
-
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-
     // Submit the form:
     f.get(0).submit(); 
   }
