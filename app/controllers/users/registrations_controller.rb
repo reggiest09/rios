38319413b734e6f230d5fbaa560bcf208f3cd8b7
Reggie Tucker <reggiest09@gmail.com>
2015-07-07 09:56:28 -0400

    flsh msg

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+before_filter :select_plan, only: :new
+
   def create
     super do |resource|
       if params[:plan]
@@ -14,5 +16,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
 
-  
+private
+def select_plan
+  unless params[:plan] && (params[:plan]== '1' || params[:plan] == '2')
+flash[:notice] = " Please Select membership plan to sign up"
+  end
+  end
 end
