c9f54382421846524629d599a3c32b168ffcbed9
Reggie Tucker <reggiest09@gmail.com>
2015-07-08 12:00:28 -0400

    profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+
+def new
+	# form where user can fill out thier own profile
+@user = User.find(params[:user_id])
+@profile = @user.build_profile
+ end
+
+def create
+@user = User.find(params[:user_id])
+@profile = @user.build_profile(profile_params)
+if @profile.save
+	flash[:success] = "Profile Updated"
+	redirect_to user_path(params[:users_id])
+	else
+  render action: :new
+	end
+end
+
+
+private
+ def profile_params
+params.require(:profile).permit(:first_name,:last_name,:phone_number,contact_email,:description,:job_title)
+ end
+
+end
