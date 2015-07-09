1ffdb3b09787751721cfe32f2e20e669fb56cf7c
Reggie Tucker <reggiest09@gmail.com>
2015-07-08 22:43:40 -0400

    before filter

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+before_action :authenticate_user!
+before_action :only_current_user
 
 def new
 	# form where user can fill out thier own profile
@@ -18,9 +20,31 @@ if @profile.save
 end
 
 
+def edit
+@user = User.find(params[:user_id])
+@profile = @user.profile
+
+end
+
+def update
+@user = User.find(params[:user_id])
+@profile = @user.profile
+if @profile.update_attributes(profile_params)
+	redirect_to user_path(params[:user_id])
+	flash[:success] = "Profile Updated"
+else
+	render action: :edit
+end
+end
+
 private
  def profile_params
 params.require(:profile).permit(:first_name, :last_name, :phone_number, :contact_email, :description, :job_title)
  end
 
+def only_current_user
+ @user = User.find(params[:user_id])
+redirect_to(root_url) unless @user == current_user
+
+end
 end
